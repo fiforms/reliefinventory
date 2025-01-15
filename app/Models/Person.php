6a96f9d252f9659a4f45f27921db5e589f8b0cfb
Daniel McFeeters <fiforms@gmail.com>
2025-01-14 23:51:30 -0500

    Created Person Model

---
+++ app/Models/Person.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'people';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The data type of the primary key.
+     *
+     * @var string
+     */
+    protected $keyType = 'int';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'organization',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+        'zip',
+        'type',
+        'comments',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'type' => 'array', // Automatically casts the SET column to an array in Laravel
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Scope a query to only include customers.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCustomers($query)
+    {
+        return $query->whereRaw("FIND_IN_SET('customer', type)");
+    }
+
+    /**
+     * Scope a query to only include donors.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDonors($query)
+    {
+        return $query->whereRaw("FIND_IN_SET('donor', type)");
+    }
+
+    /**
+     * Define relationships to other models (if applicable).
+     */
+
+    // Example relationship with OrderDonation (assuming a person can have many orders or donations)
+    public function orderDonations()
+    {
+        return $this->hasMany(OrderDonation::class, 'person_id');
+    }
+}
