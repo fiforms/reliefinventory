930ab40abbdf872c36ef7746fad9ef475415e24e
Daniel McFeeters <fiforms@gmail.com>
2025-01-16 12:37:59 -0500

    Re-styled order entry template with a global style

--- resources/js/Pages/OrderEntry.vue
+++ resources/js/Pages/OrderEntry.vue
@@ -8,10 +8,10 @@ import { Head } from '@inertiajs/vue3';
   <AuthenticatedLayout>
 	<template #header>
 	</template>
-	  <div>
-	    <div v-if="!selectedOrder">
-	      <h2>Orders</h2>
-	      <table border="1">
+	  <div class="ri_form_container">
+	    <div v-if="!selectedOrder" class="ri_datatable_container">
+	      <h2 class="ri_datatable_head">Orders</h2>
+	      <table border="1" class="ri_datatable">
 	        <thead>
 	          <tr>
 	            <th>Order Date</th>
@@ -31,18 +31,28 @@ import { Head } from '@inertiajs/vue3';
 	      </table>
 	    </div>
 	
-	    <div v-else>
-	      <h2>Order Details</h2>
-	      <p><strong>Order Date:</strong> {{ selectedOrder.order_date }}</p>
-	      <p><strong>Person:</strong> {{ selectedOrder.person.first_name }} {{ selectedOrder.person.last_name }}</p>
-	      <p><strong>Status:</strong> {{ getStatusName(selectedOrder.status_id) }}</p>
-	      <h3>Items:</h3>
-	      <ul>
-	        <li v-for="ledger in selectedOrder.item_ledgers" :key="ledger.id">
-	          {{ ledger.item.description }} - Quantity: {{ ledger.qty_subtracted }}
-	        </li>
-	      </ul>
-	      <button @click="selectedOrder = null">Back to Orders</button>
+	    <div v-else class="ri_dataform_container">
+	      <h2 class="ri_dataform_head">Order Details</h2>
+		  <table class="ri_formtable">
+			<tbody>
+		      <tr><th>Order Date:</th><td> {{ selectedOrder.order_date }}</td></tr>
+		      <tr><th>Person:</th><td> {{ selectedOrder.person.first_name }} {{ selectedOrder.person.last_name }}</td></tr>
+		      <tr><th>Status:</th><td> {{ getStatusName(selectedOrder.status_id) }}</td></tr>
+			  <tr>
+				<td colspan="2" class="ri_container_cell">
+		          <h3>Items:</h3>
+				      <table class="ri_subtable">
+						<tbody>
+				          <tr v-for="ledger in selectedOrder.item_ledgers" :key="ledger.id" class="ri_subtable_row">
+				            <td>{{ ledger.item.description }}</td><td>Quantity: {{ ledger.qty_subtracted }}</td>
+			       	      </tr >
+			     	    </tbody>
+					  </table>
+				   </td>
+			     </tr>
+			   </tbody>
+		  </table>
+	      <button @click="selectedOrder = null" class="ri_formbutton">Back to Orders</button>
 	    </div>
 	  </div>
 	</AuthenticatedLayout>
