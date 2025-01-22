ed8dca30ee71d6c69376c86347fdaeea04774dc4
Daniel McFeeters <fiforms@gmail.com>
2025-01-22 13:58:02 -0500

    Fixed output formatting for SQL

--- make_chatgpt_files.sh
+++ make_chatgpt_files.sh
@@ -2,6 +2,9 @@
 mkdir -p chatgpt
 for i in resources/js/Components/*.vue; do echo "****** $i ******"; echo ""; cat $i; echo ""; done > chatgpt/all-components.txt
 git ls-tree -r master --name-only > chatgpt/list-of-files.txt
-mysqldump -u reliefinventory -p --databases reliefinventory --compact --no-data > chatgpt/reliefinventory.sql
-sed '/\/\*/,/\*\//d' chatgpt/reliefinventory.sql > chatgpt/reliefinventory.sql.txt
-rm chatgpt/reliefinventory.sql
+mysqldump -u reliefinventory -p --databases reliefinventory --compact --no-data > chatgpt/reliefinventory.sql.txt
+sed -i -E ':a; s|/\*[^*]*\*/||g; ta'  chatgpt/reliefinventory.sql.txt   # Remove comments
+sed -i 's/^\s*;\s*$//' chatgpt/reliefinventory.sql.txt                  # Remove extra semicolons
+cp app/Http/Controllers/OrderController.php chatgpt/OrderController.php.txt
+cp routes/web.php chatgpt/routes_web.php.txt
+cp resources/js/Pages/OrderEntry.vue chatgpt/OrderEntry.vue.txt
